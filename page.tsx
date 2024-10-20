a1bbff32614a1d743ddfb25c05930709fe26fdf9
Moattar Ali <164565363+Moattar-Ali@users.noreply.github.com>
2024-10-20 23:37:49 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,23 @@
+// import styles from "./page.module.css"
+
+// export default function Responsive(){
+//     return(
+//         // <div className={styles.container}>
+//         <div className="parentContainer">
+//             <div className="childContainer"></div>
+//                 <div className="Title">BLOG TITLE</div>
+//                 <div className="imaging"></div>
+// <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum a neque ratione enim. Facere molestiae nemo alias, inventore impedit aut animi doloribus porro obcaecati amet voluptatem sed fugiat minima eveniet!
+// Pariatur enim sequi id. Minus dolores, itaque, molestiae voluptatem porro possimus enim harum, saepe vero officia fugiat tempore quae architecto velit? Voluptatem quod ab odit provident adipisci dolor debitis voluptatibus?
+// Impedit qui eaque delectus, consequatur expedita iste veritatis. Numquam commodi beatae voluptatem ab dolore doloremque quam optio veritatis deleniti. Ipsam consectetur quae iste praesentium? Nihil asperiores labore praesentium ipsum recusandae?
+// Distinctio deserunt eos id ea, minima, laudantium ab aspernatur soluta ut iste eveniet cum, esse labore quibusdam recusandae officia sint veritatis adipisci assumenda libero eius sed! Nisi ex ea inventore.
+// Sapiente voluptate, veritatis dignissimos adipisci earum beatae qui tempora quibusdam repellat tenetur quos rem ex soluta excepturi nisi non cum quod a vero! Esse velit ab amet officia voluptas sunt.</p>
+//         </div>
+
+
+
+/* //animista for animation
+ <div className="flex justify-center items-center w-[100%] h-[700px]  bg-blue-200 p-11 flex-wrap"> 
+  <div className="golaGanda sm:jello-horizontal bounce-in-right w-[90%] sm:w-[250px] h-[250px] bg-white hover:bg-blue-700 rounded-xl shadow-xl m-3"></div>
+  <div className="golaGanda sm:jello-horizontal bounce-in-right w-[90%] sm:w-[250px] h-[250px] bg-white hover:bg-blue-700 rounded-xl shadow-xl m-3"></div>
+  <div className="golaGanda sm:jello-horizontal bounce-in-right w-[90%] sm:w-[250px] h-[250px] bg-white hover:bg-blue-700 rounded-xl shadow-xl m-3">*/
